ff4a4d392f644f51024cd7939ecee92aefcdcd53
nan <nandhakumar.peee2023@sece.ac.in>
2026-01-03 11:54:12 +0530

    chore: Remove unused CSS files and clean up imports

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './styles/design-system.css';
-import './App.css';
 
 // Import Pages
 import Login from './pages/Login';
