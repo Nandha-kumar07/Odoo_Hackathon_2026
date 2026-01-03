de2ed5e1b2ec7d82d2f792e45728d77432f349bc
nan <nandhakumar.peee2023@sece.ac.in>
2026-01-03 12:20:29 +0530

    fix: Add /login route alias to fix routing warning

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -22,6 +22,7 @@ function App() {
       <div className="app">
         <Routes>
           <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/home" element={<MainLanding />} />
           <Route path="/create-trip" element={<CreateTrip />} />
